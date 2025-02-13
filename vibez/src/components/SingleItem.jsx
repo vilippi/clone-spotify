9802f42924d37bb582b44607bfcdde7c8129e81c
Vitor Lippi <vitorlippilima.vl@gmail.com>
2025-02-13 16:14:52 -0300

    Creating Components
    SingleItem and ItemList

---
+++ vibez/src/components/SingleItem.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+
+const SingleItem = () => {
+    return (
+        <div className='single-item'>
+            <div className='single-item__div-image-button'>
+                <div className='single-item__div-image'>
+                    <img
+                        className='single-item__image'
+                        src='https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4'
+                        alt='Imagem do artista'
+                    />
+                </div>
+
+                <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} />
+
+            </div>
+
+            <div className='single-item__texts'>
+                <div className='single-item__2lines'>
+                    <p className='single-item__title'>
+                        Henrique & Juliano
+                    </p>
+                </div>
+                <p className='single-item__type'>
+                    Artista
+                </p>
+            </div>
+        </div>
+    )
+};
+
+export default SingleItem;
